234dd6f764f1bb09949a29d3bc9f72d433d867d5
chenrique <caiohenriqueads@gmail.com>
2015-10-11 15:37:36 -0300

    commit inicial

---
+++ config_Android_home.sh
@@ -0,0 +1,8 @@
+#criando variavel que recebe diretorio do sdk
+ANDROID_HOME = /home/chenrique/Android/tools
+#configurando variavel android_home na variavel de amebiente
+export PATH = $PATH:/$ANDROID_HOME
+
+
+
+

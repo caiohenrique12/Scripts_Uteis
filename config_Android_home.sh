c5eaef8d0d9056e1a96a9e3d17d4803d1244a626
chenrique <caiohenriqueads@gmail.com>
2015-10-11 15:45:47 -0300

    add verificação do diretorio

--- config_Android_home.sh
+++ config_Android_home.sh
@@ -1,7 +1,13 @@
 #criando variavel que recebe diretorio do sdk
 ANDROID_HOME = /home/chenrique/Android/tools
 #configurando variavel android_home na variavel de amebiente
-export PATH = $PATH:/$ANDROID_HOME
+
+if [ -d $ANDROID_HOME ]
+then
+   export PATH = $PATH:/$ANDROID_HOME
+else
+   echo "Diretorio não existe"
+fi
 
 
 

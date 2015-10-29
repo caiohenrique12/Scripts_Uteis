5be0953fe487cd480f56a79c1d24e77c99c57488
chenrique <caiohenriqueads@gmail.com>
2015-10-29 12:15:47 -0300

    atualização

--- install_configure_rails.sh
+++ install_configure_rails.sh
@@ -1,15 +1,18 @@
 echo 'Instalando rvm com a versão do ruby mais estável'
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
-
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+sudo \curl -sSL https://get.rvm.io | bash
+echo 'Instalando Ruby com a versão mais instalvél'
+rvm install ruby-2.2.2
 versao_rvm = rvm --version
 versao_ruby = ruby --version
 
 if [ versao_rvm != null && versao_ruby != null ]
-	echo 'RVM instalado com sucesso:' + versao_rvm
-	echo 'Ruby instalado com sucesso:' + versao_ruby
+then
+   echo 'RVM instalado com sucesso:' + versao_rvm
+   echo 'Ruby instalado com sucesso:' + versao_ruby
 else
-	echo 'Falha na instalação do RVM'
-	echo 'Falha na instalação do Ruby'
+   echo 'Falha na instalação do RVM'
+   echo 'Falha na instalação do Ruby'
 fi
 
 echo 'instalando rails 4.2.2'
@@ -18,29 +21,14 @@ gem install rails --version 4.2.2 --no-ri --no-rdoc
 versao_rails = rails --version
 
 if [ versao_rails != null ]
-	echo 'Rails instalado com sucesso:' + versao_rails
+then
+    echo 'Rails instalado com sucesso:' + versao_rails
 else
-	echo 'Falha na instalação do Rails'
+    echo 'Falha na instalação do Rails'
 fi	
 
 echo 'instalando postgresql'
 
-if ["$LINUX_ARCH" == "x86_64"]; then
-	deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main
-	wget -c --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | \
-	sudo apt-key add -
-	sudo apt-get update
-
-	echo 'versão grafica'
-	wget -c http://www.enterprisedb.com/postgresql-945-installers-linux64?ls=Crossover&type=Crossover
-
-else
-	deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main
-	wget -c --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | \
-	sudo apt-key add -
-	sudo apt-get update
-
-	echo 'versão grafica'
-	wget -c http://www.enterprisedb.com/postgresql-945-installers-linux32?ls=Crossover&type=Crossover
-
-fi
+sudo add-apt-repository ppa:pitti/postgresql
+sudo apt-get update  
+sudo apt-get install postgresql

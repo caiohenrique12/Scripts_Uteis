d3364eb495530f366311de34ec7792e736bed2ac
caiohvsouza <caiohenriqueads@gmail.com>
2016-03-15 17:15:55 -0300

    Update install_configure_rails.sh

--- install_configure_rails.sh
+++ install_configure_rails.sh
@@ -1,4 +1,4 @@
-echo 'Instalando rvm com a versão do ruby mais instável'
+echo '######################## Instalando rvm com a versão do ruby mais estável ########################'
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 sudo \curl -sSL https://get.rvm.io | bash
 source /home/chenrique/.rvm/scripts/rvm
@@ -6,37 +6,37 @@ versao_rvm = rvm --version
 
 if [ versao_rvm != null ]
 then
-   echo 'RVM instalado com sucesso:' + versao_rvm
+   echo '%%%%%%%%%%%%%%% RVM instalado com sucesso: ' + versao_rvm + ' %%%%%%%%%%%%%%%'
 else
-   echo 'Falha na instalação do RVM'
+   echo '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Falha na instalação do RVM @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
 fi
 
-echo 'Instalando Ruby com a versão mais instável'
-rvm install ruby-2.2.3
+echo '######################## Instalando Ruby com a versão mais estável ########################'
+rvm install ruby-2.3.0
 versao_ruby = ruby --version
 
 if [ versao_ruby != null ]
 then
-   echo 'Ruby instalado com sucesso:' + versao_ruby
+   echo '%%%%%%%%%%%%%%% Ruby instalado com sucesso:' + versao_ruby + ' %%%%%%%%%%%%%%%'
 else
-   echo 'Falha na instalação do Ruby'
+   echo '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Falha na instalação do Ruby @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
 fi
 
 sudo apt-get install libxslt-dev libxml2-dev
 sudo apt-get install build-essential
 sudo apt-get install libgmp3-dev
 
-echo 'instalando rails 4.2.2'
+echo '######################## instalando rails mais estável ########################'
 gem install rails --version 4.2.4 --no-ri --no-rdoc
 
 versao_rails = rails --version
 
 if [ versao_rails != null ]
 then
-    echo 'Rails instalado com sucesso:' + versao_rails
+    echo '%%%%%%%%%%%%%%% Rails instalado com sucesso:' + versao_rails + ' %%%%%%%%%%%%%%%'
 else
-    echo 'Falha na instalação do Rails'
+    echo '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Falha na instalação do Rails @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
 fi	
 
-echo 'Instalando lib para o postgres'
+echo '######################## Instalando lib para o postgres ########################'
 sudo apt-get install libpq-dev
